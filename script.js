2a55e5410e7b3bb180c57b9dbe5c6f47867d8f03
kseroxl <leonowa.kseni@gmail.com>
2019-07-13 20:02:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,11 +19,12 @@ var subm = document.getElementById("ok");
 var PlayerOne = document.getElementsByClassName("pl")[0];
 var PlayerTwo = document.getElementsByClassName("pl")[1];
 
-PlayerOne.addEventListener("input", function() { 
+PlayerOne.addEventListener("change", function() { 
     document.getElementById("name1").textContent = this.value;
 })
 
-PlayerTwo.addEventListener("input", function() {
+PlayerOne.addEventListener("change", function() { 
+    PlayerTwo.addEventListener("input", function() {
     document.getElementById("name2").textContent = this.value;
 })
 
